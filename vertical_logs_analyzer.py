43608aa453e32671d4dcc04643f3bffbd2964d1d
Bartosz Sroka <srokab019@gmail.com>
2023-04-24 17:37:40 +0200

    Added polynomial method to class VerticalLogsAnalyzer

--- vertical_logs_analyzer.py
+++ vertical_logs_analyzer.py
@@ -5,6 +5,9 @@ import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy
+from scipy.interpolate import CubicSpline
+from matplotlib.patches import Rectangle
 from typing import Tuple, List, Any, Dict
 
 warnings.filterwarnings("ignore")
@@ -91,7 +94,8 @@ class VerticalLogsAnalyzer:
             None
         """
         self.minimum_start_timestamp = self.analyzed_logs[self.start_timestamp_column_name].min() - dt.timedelta(10)
-        self.maximum_complete_timestamp = self.analyzed_logs[self.complete_timestamp_column_name].max() + dt.timedelta(10)
+        self.maximum_complete_timestamp = self.analyzed_logs[self.complete_timestamp_column_name].max() + dt.timedelta(
+            10)
 
     def moved_logs_to_side(self) -> None:
         """
@@ -129,6 +133,114 @@ class VerticalLogsAnalyzer:
         self.analyzed_logs = self.analyzed_logs.drop('diff', axis=1)
 
         self.update_range_of_timestamp()
+
+    def plot_task_duration(self, df: pd.DataFrame, ax: plt.Axes, color: Any = 'r', label=''):
+        """
+        Generate a plot of one task logs, like a line in time.
+
+        :param:
+            df (pd.DataFrame): The dataframe containing the task logs.
+            ax (plt.Axes): The Matplotlib Axes object to plot on.
+            color (Any, optional): The color to use for the plot. Defaults to 'r'.
+            label (str, optional): The label for the plot. Defaults to ''.
+
+        :return:
+            None
+        """
+        for i in range(df.shape[0] - 1):
+            ax.plot([np.array(df[self.start_timestamp_column_name])[i],
+                     np.array(df[self.complete_timestamp_column_name])[i]],
+                    [np.array(df[self.case_id_column_name])[i],
+                     np.array(df[self.case_id_column_name])[i]], color=color, alpha=0.5,
+                    label=label)
+        ax.get_yaxis().set_visible(False)
+        ax.set(xlim=[self.minimum_start_timestamp, self.maximum_complete_timestamp])
+
+    def plot_all_tasks(self, colors: List = None):
+        """
+        Generates plot_task_duration for all tasks in logs
+
+        :param
+            colors: A list of colors to use for each task. If not provided, generates random colors.
+        """
+        if not colors:
+            colors = [generate_random_color() for _ in range(len(self.list_of_tasks))]
+
+        fig, ax = plt.subplots(figsize=[14, 6])
+        list_of_tasks = list(self.list_of_tasks)
+        for task in list_of_tasks:
+            self.plot_task_duration(
+                df=self.logs.loc[self.logs[self.task_column_name] == task],
+                ax=ax,
+                color=colors[list_of_tasks.index(task)],
+                label=task)
+
+        handles, labels = ax.get_legend_handles_labels()
+        unique_labels = list(set(labels))
+        legend_handles = []
+        legend_labels = []
+        for label in unique_labels:
+            index = labels.index(label)
+            legend_handles.append(handles[index])
+            legend_labels.append(labels[index])
+        plt.legend(legend_handles, legend_labels)
+        plt.tight_layout()
+        plt.show()
+
+    def plot_tasks(self, colors: List = None):
+        """
+        Generates plot_task_duration for two analyzed tasks.
+
+        :param:
+            colors: A list of colors to use for each task. If not provided, generates random colors.
+        """
+        if not colors:
+            colors = [generate_random_color() for _ in range(len(self.list_of_tasks))]
+
+        fig, ax = plt.subplots(figsize=[14, 6])
+        list_of_tasks = list(self.select_time_order_tasks())
+        for task in list_of_tasks:
+            self.plot_task_duration(
+                df=self.analyzed_logs.loc[self.analyzed_logs[self.task_column_name] == task],
+                ax=ax,
+                color=colors[list_of_tasks.index(task)],
+                label=task)
+
+        handles, labels = ax.get_legend_handles_labels()
+        unique_labels = list(set(labels))
+        legend_handles = []
+        legend_labels = []
+        for label in unique_labels:
+            index = labels.index(label)
+            legend_handles.append(handles[index])
+            legend_labels.append(labels[index])
+        plt.legend(legend_handles, legend_labels)
+        plt.tight_layout()
+        plt.show()
+
+    def print_results(self, area_first_task, area_second_task, area_splot):
+        """
+        Prints the results of area calculations and percentage ratios.
+
+        :param:
+            area_first_task (float): area of the first task
+            area_second_task (float): area of the second task
+            area_splot (float): area of the splot
+        :return:
+            None
+        """
+        first_task, second_task = self.select_time_order_tasks()
+        print(45 * "= ")
+        print("{0} area:\033[1m {1} \033[0m".format(first_task, round(area_first_task, 2)))
+        print("{0} area:\033[1m {1} \033[0m".format(second_task, round(area_second_task, 2)))
+        print("splot area:\033[1m {} \033[0m".format(round(area_splot, 2)))
+        print("\nsplot/task1:\033[1m {} % \033[0m".format(round(100 * area_splot / area_first_task), 2))
+        print("splot/task2:\033[1m {} % \033[0m".format(round(100 * area_splot / area_second_task), 2))
+        print("")
+
+    #
+    #  TRAPEZOID METHODS
+    #
 
     def calculate_trapezoid_metrics(self) -> Dict[str, Any]:
         """
@@ -168,7 +280,7 @@ class VerticalLogsAnalyzer:
         D = (stats['mean_complete'] + stats['std_complete'], 0)
         return [A, B, C, D]
 
-    def calculate_timestamp_metrics(self, plot_result: bool = False, is_big_dataframe: bool = False):
+    def calculate_trapezoid_timestamp_metrics(self, plot_result: bool = False, is_big_dataframe: bool = False):
         """
         Calculates the area under the curves for two tasks and their intersection.
         The trapezoid function is calculated for each task using the mean start and
@@ -206,137 +318,14 @@ class VerticalLogsAnalyzer:
 
         return area_first_task, area_second_task, area_splot
 
-    def print_results(self, area_first_task, area_second_task, area_splot):
-        """
-        Prints the results of area calculations and percentage ratios.
-
-        :param:
-            area_first_task (float): area of the first task
-            area_second_task (float): area of the second task
-            area_splot (float): area of the splot
-        :return:
-            None
-        """
-        first_task, second_task = self.select_time_order_tasks()
-        print(45 * "= ")
-        print("{0} area:\033[1m {1} \033[0m".format(first_task, round(area_first_task, 2)))
-        print("{0} area:\033[1m {1} \033[0m".format(second_task, round(area_second_task, 2)))
-        print("splot area:\033[1m {} \033[0m".format(round(area_splot, 2)))
-        print("\nsplot/task1:\033[1m {} % \033[0m".format(round(100 * area_splot / area_first_task), 2))
-        print("splot/task2:\033[1m {} % \033[0m".format(round(100 * area_splot / area_second_task), 2))
-        print("")
-
-    def plot_task_duration(self, df: pd.DataFrame, ax: plt.Axes, color: Any = 'r', label=''):
-        """
-        Generate a plot of one task logs, like a line in time.
-
-        :param:
-            df (pd.DataFrame): The dataframe containing the task logs.
-            ax (plt.Axes): The Matplotlib Axes object to plot on.
-            color (Any, optional): The color to use for the plot. Defaults to 'r'.
-            label (str, optional): The label for the plot. Defaults to ''.
-
-        :return:
-            None
-        """
-        for i in range(df.shape[0] - 1):
-            ax.plot([np.array(df[self.start_timestamp_column_name])[i],
-                     np.array(df[self.complete_timestamp_column_name])[i]],
-                    [np.array(df[self.case_id_column_name])[i],
-                     np.array(df[self.case_id_column_name])[i]], color=color, alpha=0.5,
-                    label=label)
-        ax.get_yaxis().set_visible(False)
-        ax.set(xlim=[self.minimum_start_timestamp, self.maximum_complete_timestamp])
-
-    def plot_tasks(self, colors: List = None):
-        """
-        Generates plot_task_duration for two analyzed tasks.
-
-        :param:
-            colors: A list of colors to use for each task. If not provided, generates random colors.
-        """
-        if not colors:
-            colors = [generate_random_color() for _ in range(len(self.list_of_tasks))]
-
-        fig, ax = plt.subplots(figsize=[14, 6])
-        list_of_tasks = list(self.select_time_order_tasks())
-        for task in list_of_tasks:
-            self.plot_task_duration(
-                df=self.analyzed_logs.loc[self.analyzed_logs[self.task_column_name] == task],
-                ax=ax,
-                color=colors[list_of_tasks.index(task)],
-                label=task)
-
-        handles, labels = ax.get_legend_handles_labels()
-        unique_labels = list(set(labels))
-        legend_handles = []
-        legend_labels = []
-        for label in unique_labels:
-            index = labels.index(label)
-            legend_handles.append(handles[index])
-            legend_labels.append(labels[index])
-        plt.legend(legend_handles, legend_labels)
-        plt.tight_layout()
-        plt.show()
-
-    def plot_all_tasks(self, colors: List = None):
-        """
-        Generates plot_task_duration for all tasks in logs
-
-        :param
-            colors: A list of colors to use for each task. If not provided, generates random colors.
-        """
-        if not colors:
-            colors = [generate_random_color() for _ in range(len(self.list_of_tasks))]
-
-        fig, ax = plt.subplots(figsize=[14, 6])
-        list_of_tasks = list(self.list_of_tasks)
-        for task in list_of_tasks:
-            self.plot_task_duration(
-                df=self.logs.loc[self.logs[self.task_column_name] == task],
-                ax=ax,
-                color=colors[list_of_tasks.index(task)],
-                label=task)
-
-        handles, labels = ax.get_legend_handles_labels()
-        unique_labels = list(set(labels))
-        legend_handles = []
-        legend_labels = []
-        for label in unique_labels:
-            index = labels.index(label)
-            legend_handles.append(handles[index])
-            legend_labels.append(labels[index])
-        plt.legend(legend_handles, legend_labels)
-        plt.tight_layout()
-        plt.show()
-
-    def plot_trapezoid(self, tasks_function_range, tasks_function_values):
-        """
-        Plots the trapezoid function for two tasks.
-
-        :param:
-            tasks_function_range (np.ndarray): A numpy array containing the range of the tasks function.
-            tasks_function_values (Dict): A dictionary containing the values of the tasks functions for each task.
-
-        :return:
-            None
-        """
-        first_task, second_task = self.select_time_order_tasks()
-        _, ax = plt.subplots(nrows=1, ncols=1, figsize=[14, 6])
-        ax.plot(tasks_function_range, tasks_function_values[first_task], 'r--')
-        ax.plot(tasks_function_range, tasks_function_values[second_task], 'b--')
-        plt.legend(self.select_time_order_tasks())
-        plt.tight_layout()
-        plt.show()
-
-    def core_metod(self,
-                   first_task: str,
-                   second_task: str,
-                   instance: str = None,
-                   plot_steps: bool = False,
-                   plot_results: bool = False,
-                   print_results: bool = False,
-                   is_big_dataframe: bool = False):
+    def core_trapezoid_method(self,
+                              first_task: str,
+                              second_task: str,
+                              instance: str = None,
+                              plot_steps: bool = False,
+                              plot_results: bool = False,
+                              print_results: bool = False,
+                              is_big_dataframe: bool = False):
         """
         Perform core analysis for trapezoid method on the provided data and return a possible
         relation between the two tasks.
@@ -359,8 +348,10 @@ class VerticalLogsAnalyzer:
         self.moved_logs_to_side()
         if plot_steps:
             self.plot_tasks(colors=['red', 'blue'])
-        area_first_task, area_second_task, area_splot = self.calculate_timestamp_metrics(plot_result=plot_results,
-                                                                                         is_big_dataframe=is_big_dataframe)
+        area_first_task, area_second_task, area_splot = self.calculate_trapezoid_timestamp_metrics(
+            plot_result=plot_results,
+            is_big_dataframe=is_big_dataframe)
+
         if area_first_task == 0 or area_second_task == 0:
             print(45 * "= ")
             print("{0} area:\033[1m {1} \033[0m".format(first_task, round(area_first_task, 2)))
@@ -368,6 +359,144 @@ class VerticalLogsAnalyzer:
             return []
         if print_results: self.print_results(area_first_task, area_second_task, area_splot)
         return return_possible_relation(area_splot / area_first_task, area_splot / area_second_task)
+
+    def plot_trapezoid(self, tasks_function_range, tasks_function_values):
+        """
+        Plots the trapezoid function for two tasks.
+
+        :param:
+            tasks_function_range (np.ndarray): A numpy array containing the range of the tasks function.
+            tasks_function_values (Dict): A dictionary containing the values of the tasks functions for each task.
+
+        :return:
+            None
+        """
+        first_task, second_task = self.select_time_order_tasks()
+        _, ax = plt.subplots(nrows=1, ncols=1, figsize=[14, 6])
+        ax.plot(tasks_function_range, tasks_function_values[first_task], 'r--')
+        ax.plot(tasks_function_range, tasks_function_values[second_task], 'b--')
+        plt.legend(self.select_time_order_tasks())
+        plt.tight_layout()
+        plt.show()
+
+    #
+    # POLYNOMIAL METHOD
+    #
+
+    def combine_task_intervals(self, task: str) -> Tuple[np.ndarray, np.ndarray]:
+        df = self.analyzed_logs[self.analyzed_logs[self.task_column_name] == task].reset_index()
+        timestamps = np.unique(np.array(
+            sorted(list(df[self.start_timestamp_column_name]) + list(df[self.complete_timestamp_column_name]))))
+        frequencies = np.array([0] * len(timestamps))
+
+        for i in range(len(df)):
+            start_ts = df[self.start_timestamp_column_name][i]
+            complete_ts = df[self.complete_timestamp_column_name][i]
+            start_idx = list(timestamps).index(start_ts)
+            complete_idx = list(timestamps).index(complete_ts)
+
+            for j in range(start_idx, complete_idx + 1):
+                frequencies[j] = frequencies[j] + 1
+
+        return np.array([t.timestamp() for t in timestamps]), frequencies
+
+    def calculate_polynomial_timestamp_metrics(self,
+                                               task1,
+                                               task2,
+                                               timestamps1,
+                                               frequencies1,
+                                               timestamps2,
+                                               frequencies2,
+                                               colors,
+                                               plot_result: bool = False) -> Tuple[float, float, float]:
+        if plot_result:
+            fig = plt.figure(figsize=(30, 10))
+
+        task_list = [task1, task2]
+        timestamps_list = [timestamps1, timestamps2]
+        frequencies_list = [frequencies1, frequencies2]
+        approx_list = []
+        area_list = []
+
+        for i in range(2):
+            if plot_result:
+                ax = fig.add_subplot(1, 3, i + 1)
+
+            task = task_list[i]
+            timestamps = timestamps_list[i]
+            frequencies = frequencies_list[i]
+            N = len(timestamps)
+            approx = CubicSpline(np.array(timestamps), np.array(frequencies), bc_type='natural')
+            approx_list.append(approx)
+            area_rect = sum([((timestamps[i + 1] - timestamps[i]) * frequencies[i]) for i in range(N - 1)])
+            area_poly = approx.integrate(timestamps[0], timestamps[-1])
+            area_list.append(area_poly)
+
+            if plot_result:
+                for j in range(N - 1):
+                    t1 = timestamps[j]
+                    t2 = timestamps[j + 1]
+                    freq = frequencies[j]
+                    ax.add_patch(Rectangle((t1, 0), t2 - t1, freq, color=colors[i], label=task))
+
+                ax.plot(timestamps, approx(np.array(timestamps)), label="Approximation")
+                ax.set_xlim([min(timestamps), max(timestamps)])
+                ax.set_title(
+                    f"Area calculated by rectangles: {area_rect}\nArea calculate by quad: {area_poly}\nRatio: {area_rect / area_poly}")
+
+        t1 = (timestamps_list[0][0], timestamps_list[0][-1])
+        t2 = (timestamps_list[1][0], timestamps_list[1][-1])
+        conv_fun = lambda x: min([approx_list[0](x), approx_list[1](x)])
+        area1 = float(area_list[0])
+        area2 = float(area_list[1])
+        area_conv = scipy.integrate.quad(conv_fun, max([t1[0], t2[0]]), min([t1[1], t2[1]]))[0]
+
+        if plot_result:
+            ax = fig.add_subplot(1, 3, 3)
+            ax.plot(timestamps_list[0], approx_list[0](timestamps_list[0]), label=task_list[0])
+            ax.plot(timestamps_list[1], approx_list[1](timestamps_list[1]), label=task_list[1])
+            # show_legend(fig)
+            plt.show()
+
+        return area1, area2, area_conv
+
+    def core_polynomial_method(self,
+                               first_task: str,
+                               second_task: str,
+                               instance: str = None,
+                               plot_steps: bool = False,
+                               plot_results: bool = False,
+                               print_results: bool = False) -> List[str]:
+
+        self.select_data_for_analyze(first_task, second_task, instance=instance)
+
+        if plot_steps:
+            colors = ['red', 'blue']
+            self.plot_tasks(colors)
+            self.moved_logs_to_side()
+            self.plot_tasks(colors)
+
+        else:
+            self.moved_logs_to_side()
+
+        timestamps1, frequencies1 = self.combine_task_intervals(first_task)
+        timestamps2, frequencies2 = self.combine_task_intervals(second_task)
+        area1, area2, area_conv = self.calculate_polynomial_timestamp_metrics(first_task,
+                                                                              second_task,
+                                                                              timestamps1,
+                                                                              frequencies1,
+                                                                              timestamps2,
+                                                                              frequencies2,
+                                                                              ['red', 'blue'],
+                                                                              plot_result=plot_results)
+
+        if print_results:
+            self.print_results(area1, area2, area_conv)
+
+        return return_possible_relation(area_conv / area1, area_conv / area2)
+
+    def plot_polynomial(self):
+        pass
 
 
 def generate_function(points):
